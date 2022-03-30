2c5b7144db6b59778f63a850555b4191a85d5dc4
yurrim <yurimkang20@gmail.com>
2022-03-30 22:59:15 +0900

    ascending

--- Sorting/HeapSort1.java
+++ Sorting/HeapSort1.java
@@ -2,24 +2,24 @@ package Sorting;
 
 import java.util.Arrays;
 
-// FIX 0 부터 노드 계산되도록 수정 필요
-
 public class HeapSort1 {
+    
+    // 0이 아닌 1부터 시작하는 배열 정렬 
 
     public static void HeapSort(int unSorted[]) {
 
-        int n = unSorted.length - 1;
+        int n = unSorted.length - 1;        // 9
         int temp = 0;
 
-        for (int i = n/2; i >= 1; i--) {
+        for (int i = n/2; i >= 1; i--) {    // 4 부터
             Heapify(unSorted, i, n);
         }
 
         for (int i = n-1; i >= 1; i--) {
-            temp = unSorted[0];
-            unSorted[0] = unSorted[i+1];
+            temp = unSorted[1];
+            unSorted[1] = unSorted[i+1];
             unSorted[i+1] = temp;
-            Heapify(unSorted, 0, i);
+            Heapify(unSorted, 1, i);
         }
         
     }
@@ -49,7 +49,7 @@ public class HeapSort1 {
         
         StringBuilder sb = new StringBuilder();
 
-        int[] unSortedArray = {13, 7, 9, 3, 11, 6, 12};
+        int[] unSortedArray = {0, 6, 2, 8, 1, 3, 9, 4, 5, 10, 7};
         HeapSort(unSortedArray);
 
         sb.append(Arrays.toString(unSortedArray));
