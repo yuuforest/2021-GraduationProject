cea6888f4132dd0972c70559a7098e7ad82de2b4
yurrim <yurimkang20@gmail.com>
2022-04-12 18:39:47 +0900

    Knuth-Morris-Pratt

--- String_Searching/KMP.java
+++ String_Searching/KMP.java
@@ -13,7 +13,16 @@ public class KMP {
         next[0] = -1;
 
         for (int i = 0, j = -1; i < plength; i++, j++) {
+            
+            // Original
             next[i] = j;
+
+            // Better
+            // if (j != -1) {
+            //     next[i] = ((pattern.charAt(i) == pattern.charAt(j)) ? next[j] : j);
+            // } else {
+            //     next[i] = j;
+            // }
 
             while ((j >= 0) && (pattern.charAt(i) != pattern.charAt(j))) {
                 j = next[j];
@@ -23,17 +32,38 @@ public class KMP {
         return next;
     }
 
+    public static int kmp(String pattern, String text) {
+        
+        int plength = pattern.length();
+        int tlength = text.length();
+
+        int[] next = initNext(pattern);
+        int i, j;
+
+        for (i = 0, j = 0; j < plength && i < tlength; i++, j++) {
+            while ((j >= 0) && (text.charAt(i) != pattern.charAt(j))) {
+                j = next[j];
+            }
+        }
+
+        if (j == plength) return (i - plength);
+        else return i;
+    }
 
     public static void main(String[] args) {
 
         StringBuilder sb = new StringBuilder();
 
-        // String text = "ababacabacaaba";
-        String pattern = "10100111";
+        String text = "ababacabacaaba";
+        String pattern = "caba";
 
-        int[] nextTable = initNext(pattern);
+        // initNext 함수로 Next 배열 출력
+        // int[] nextTable = initNext(pattern);
+        // sb.append(Arrays.toString(nextTable));
 
-        sb.append(Arrays.toString(nextTable));
+        // KMP
+        int matching = kmp(pattern, text);
+        sb.append("text[" + matching + "] = " + text.charAt(matching));
 
         System.out.println(sb);
 
